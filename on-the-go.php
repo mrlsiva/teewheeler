a35ef2a3c2c163d18613280b5e490ab99289674a
siva <pmrlsivas@gmail.com>
2024-04-23 18:54:13 +0530

    footer update

--- on-the-go.php
+++ on-the-go.php
@@ -255,98 +255,7 @@ color: #fff;
             <img src="img/footer-top.jpg" alt="1" class="img-fluid"/>
             
             </div>-->
-         <div class="footer ptb-30">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-4"></div>
-                  <div class="col-md-4 col-xs-12" >
-                     <h4 class="bs fot-top-cnt brd-10"><a href="order-online.php" class="clr-wht">Order Online Now
-                        !</a>
-                     </h4>
-                  </div>
-                  <div class="col-md-4"></div>
-               </div>
-               <div class="row">
-                  <div class="col-md-4 col-xs-12 mb-1 ft-tc">
-                     <img src="img/logo.png" width="100"
-                        class="img-fluid"><br>
-                     <img src="img/txt-logo.png" width="100"
-                        class="img-fluid">
-                  </div>
-                  <div class="col-md-4 col-xs-12 mb-1 pt-3 ft-tc">
-                     <div class="row">
-                        <div class="col-md-6">
-                           <ul class="ord-ul fmenu">
-                              <li><a href="index.php">Home</a></li>
-                              <li><a href="for-events.php">Events</a></li>
-                              <li><a href="msme.php">MSME</a></li>
-                           </ul>
-                        </div>
-                        <div class="col-md-6">
-                           <ul class="ord-ul fmenu">
-                              <li><a href="order-online.php">Order Online</a></li>
-                              <li><a href="on-the-go.php">On The Go</a></li>
-                              <li><a href="contact.php">Contact</a></li>
-                           </ul>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-md-4 col-xs-12 ft-tc">
-                     <ul class="ord-ul lh-30  pt-0 fcnt">
-                        <!--pl-5-->
-                        <li>
-                           <svg class="svg-inline--fa fa-envelope fa-w-16 fa-lg sicon mr-2" aria-hidden="true"
-                              data-prefix="fa" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 512 512" data-fa-i2svg="">
-                              <path fill="currentColor"
-                                 d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z">
-                              </path>
-                           </svg>
-                           <!-- <i class="fa fa-envelope fa-lg sicon mr-2"></i> --> <a
-                              href="">Connect@Teawheeler.co.in</a>
-                        </li>
-                        <li>
-                           <svg class="svg-inline--fa fa-phone-square fa-w-14 fa-lg sicon mr-2" aria-hidden="true"
-                              data-prefix="fa" data-icon="phone-square" role="img"
-                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                              <path fill="currentColor"
-                                 d="M400 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zM94 416c-7.033 0-13.057-4.873-14.616-11.627l-14.998-65a15 15 0 0 1 8.707-17.16l69.998-29.999a15 15 0 0 1 17.518 4.289l30.997 37.885c48.944-22.963 88.297-62.858 110.781-110.78l-37.886-30.997a15.001 15.001 0 0 1-4.289-17.518l30-69.998a15 15 0 0 1 17.16-8.707l65 14.998A14.997 14.997 0 0 1 384 126c0 160.292-129.945 290-290 290z">
-                              </path>
-                           </svg>
-                           <!-- <i class="fa fa-phone-square fa-lg sicon mr-2"></i> --> <a href="">+91 91769
-                           69999</a>
-                        </li>
-                     </ul>
-                     <p class="fmenu lh-30 mt-2">
-                        <a href="https://www.instagram.com/tea_wheeler/">
-                            <svg
-                                class="svg-inline--fa fa-instagram fa-w-14 fa-lg sicon mr-10" aria-hidden="true"
-                                data-prefix="fab" data-icon="instagram" role="img"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                                <path fill="currentColor"
-                                    d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
-                                </path>
-                            </svg>
-                            <!-- <i class="fab fa-instagram fa-lg sicon mr-10"></i> -->
-                        </a>
-                        
-                        <a
-                            href="https://www.facebook.com/teawheelers">
-                            <svg
-                                class="svg-inline--fa fa-facebook fa-w-14 fa-lg sicon mr-10" aria-hidden="true"
-                                data-prefix="fab" data-icon="facebook" role="img" xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 448 512" data-fa-i2svg="">
-                                <path fill="currentColor"
-                                    d="M448 56.7v398.5c0 13.7-11.1 24.7-24.7 24.7H309.1V306.5h58.2l8.7-67.6h-67v-43.2c0-19.6 5.4-32.9 33.5-32.9h35.8v-60.5c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9h-58.4v67.6h58.4V480H24.7C11.1 480 0 468.9 0 455.3V56.7C0 43.1 11.1 32 24.7 32h398.5c13.7 0 24.8 11.1 24.8 24.7z">
-                                </path>
-                            </svg>
-                            <!-- <i class="fab fa-facebook fa-lg sicon mr-10"></i> -->
-                       
-                        </p>
-                  </div>
-               </div>
-            </div>
-         </div>
+            <div class="footer ptb-30" id="footer"> </div>
       </section>
       <!--footer part end-->
       <!-- Optional JavaScript -->
@@ -410,5 +319,11 @@ color: #fff;
 	 <script src="js/jquery.slim.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#footer").load("footer.html");
+        });
+    </script>
    </body>
 </html>
