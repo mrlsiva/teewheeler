4121bb595cb5d2201638fdaf33bbed35517e38e5
siva <pmrlsivas@gmail.com>
2024-04-23 18:29:14 +0530

    fb update

--- msme.php
+++ msme.php
@@ -17,126 +17,7 @@
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
    </head>
    <body class="abt-bg">
-      <script>__wm.rw(0);</script>
-      <div id="wm-ipp-base" lang="en" style="display:none;direction:ltr;">
-         <div id="wm-ipp" style="position:fixed;left:0;top:0;right:0;">
-            <div id="donato" style="position:relative;width:100%;">
-               <div id="donato-base">
-                  <iframe id="donato-if" src="https://archive.org/includes/donate.php?as_page=1&amp;platform=wb&amp;referer=https%3A//web.archive.org/web/20230223130814/http%3A//www.teawheeler.co.in/msme.php"
-                     scrolling="no" frameborder="0" style="width:100%; height:100%">
-                  </iframe>
-               </div>
-            </div>
-            <div id="wm-ipp-inside">
-               <div id="wm-toolbar" style="position:relative;display:flex;flex-flow:row nowrap;justify-content:space-between;">
-                  <div id="wm-logo" style="/*width:110px;*/padding-top:12px;">
-                     <a href="/web/" title="Wayback Machine home page"><img src="https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-200.png" srcset="https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-100.png, https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-150.png 1.5x, https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-200.png 2x" alt="Wayback Machine" style="width:100px" border="0" /></a>
-                  </div>
-                  <div class="c" style="display:flex;flex-flow:column nowrap;justify-content:space-between;flex:1;">
-                     <form class="u" style="display:flex;flex-direction:row;flex-wrap:nowrap;" target="_top" method="get" action="/web/submit" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="http://www.teawheeler.co.in/msme.php" onfocus="this.focus();this.select();" style="flex:1;"/><input type="hidden" name="type" value="replay" /><input type="hidden" name="date" value="20230223130814" /><input type="submit" value="Go" />
-                     </form>
-                     <div style="display:flex;flex-flow:row nowrap;align-items:flex-end;">
-                        <div class="s" id="wm-nav-captures" style="flex:1;">
-                           <a class="t" href="/web/20230223130814*/http://www.teawheeler.co.in/msme.php" title="See a list of every capture for this URL">2 captures</a>
-                           <div class="r" title="Timespan for captures of this URL">23 Feb 2023 - 23 Feb 2023</div>
-                        </div>
-                        <div class="k">
-                           <a href="" id="wm-graph-anchor">
-                              <div id="wm-ipp-sparkline" title="Explore captures for this URL" style="position: relative">
-                                 <canvas id="wm-sparkline-canvas" width="725" height="27" border="0"></canvas>
-                              </div>
-                           </a>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="n">
-                     <table>
-                        <tbody>
-                           <!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
-                           <tr class="m">
-                              <td class="b" nowrap="nowrap">Jan</td>
-                              <td class="c" id="displayMonthEl" title="You are here: 13:08:14 Feb 23, 2023">FEB</td>
-                              <td class="f" nowrap="nowrap">Mar</td>
-                           </tr>
-                           <!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
-                           <tr class="d">
-                              <td class="b" nowrap="nowrap"><img src="https://web-static.archive.org/_static/images/toolbar/wm_tb_prv_off.png" alt="Previous capture" width="14" height="16" border="0" /></td>
-                              <td class="c" id="displayDayEl" style="width:34px;font-size:22px;white-space:nowrap;" title="You are here: 13:08:14 Feb 23, 2023">23</td>
-                              <td class="f" nowrap="nowrap"><img src="https://web-static.archive.org/_static/images/toolbar/wm_tb_nxt_off.png" alt="Next capture" width="14" height="16" border="0" /></td>
-                           </tr>
-                           <!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
-                           <tr class="y">
-                              <td class="b" nowrap="nowrap">2022</td>
-                              <td class="c" id="displayYearEl" title="You are here: 13:08:14 Feb 23, 2023">2023</td>
-                              <td class="f" nowrap="nowrap">2024</td>
-                           </tr>
-                        </tbody>
-                     </table>
-                  </div>
-                  <div class="r" style="display:flex;flex-flow:column nowrap;align-items:flex-end;justify-content:space-between;">
-                     <div id="wm-btns" style="text-align:right;height:23px;">
-                        <span class="xxs">
-                           <div id="wm-save-snapshot-success">success</div>
-                           <div id="wm-save-snapshot-fail">fail</div>
-                           <a id="wm-save-snapshot-open" href="#" title="Share via My Web Archive" >
-                           <span class="iconochive-web"></span>
-                           </a>
-                           <a href="https://archive.org/account/login.php" title="Sign In" id="wm-sign-in">
-                           <span class="iconochive-person"></span>
-                           </a>
-                           <span id="wm-save-snapshot-in-progress" class="iconochive-web"></span>
-                        </span>
-                        <a class="xxs" href="http://faq.web.archive.org/" title="Get some help using the Wayback Machine" style="top:-6px;"><span class="iconochive-question" style="color:rgb(87,186,244);font-size:160%;"></span></a>
-                        <a id="wm-tb-close" href="#close" style="top:-2px;" title="Close the toolbar"><span class="iconochive-remove-circle" style="color:#888888;font-size:240%;"></span></a>
-                     </div>
-                     <div id="wm-share" class="xxs">
-                        <a href="/web/20230223130814/http://web.archive.org/screenshot/http://www.teawheeler.co.in/msme.php"
-                           id="wm-screenshot"
-                           title="screenshot">
-                        <span class="wm-icon-screen-shot"></span>
-                        </a>
-                        <a href="#" id="wm-video" title="video">
-                        <span class="iconochive-movies"></span>
-                        </a>
-                        <a id="wm-share-facebook" href="#" data-url="https://web.archive.org/web/20230223130814/http://www.teawheeler.co.in/msme.php" title="Share on Facebook" style="margin-right:5px;" target="_blank"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
-                        <a id="wm-share-twitter" href="#" data-url="https://web.archive.org/web/20230223130814/http://www.teawheeler.co.in/msme.php" title="Share on Twitter" style="margin-right:5px;" target="_blank"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
-                     </div>
-                     <div style="padding-right:2px;text-align:right;white-space:nowrap;">
-                        <a id="wm-expand" class="wm-btn wm-closed" href="#expand" onclick="__wm.ex(event);return false;"><span id="wm-expand-icon" class="iconochive-down-solid"></span> <span class="xxs" style="font-size:80%;">About this capture</span></a>
-                     </div>
-                  </div>
-               </div>
-               <div id="wm-capinfo" style="border-top:1px solid #777;display:none; overflow: hidden">
-                  <div id="wm-capinfo-notice" source="api"></div>
-                  <div id="wm-capinfo-collected-by">
-                     <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center">COLLECTED BY</div>
-                     <div style="padding:3px;position:relative" id="wm-collected-by-content">
-                        <div style="display:inline-block;vertical-align:top;width:49%;">
-                           <span class="c-logo" style="background-image:url(https://archive.org/services/img/certificate-transparency)"></span>
-                           <div>Collection: <a style="color:#33f;" href="https://archive.org/details/certificate-transparency" target="_new"><span class="wm-title">Certificate Transparency</span></a></div>
-                        </div>
-                     </div>
-                  </div>
-                  <div id="wm-capinfo-timestamps">
-                     <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center" title="Timestamps for the elements of this page">TIMESTAMPS</div>
-                     <div>
-                        <div id="wm-capresources" style="margin:0 5px 5px 5px;max-height:250px;overflow-y:scroll !important"></div>
-                        <div id="wm-capresources-loading" style="text-align:left;margin:0 20px 5px 5px;display:none"><img src="https://web-static.archive.org/_static/images/loading.gif" alt="loading" /></div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <div id="wm-ipp-print">The Wayback Machine - https://web.archive.org/web/20230223130814/http://www.teawheeler.co.in/msme.php</div>
-      <script type="text/javascript">//<![CDATA[
-         __wm.bt(725,27,25,2,"web","http://www.teawheeler.co.in/msme.php","20230223130814",1996,"https://web-static.archive.org/_static/",["https://web-static.archive.org/_static/css/banner-styles.css?v=S1zqJCYt","https://web-static.archive.org/_static/css/iconochive.css?v=qtvMKcIJ"], false);
-           __wm.rw(1);
-         //]]>
-      </script>
-      <!-- END WAYBACK TOOLBAR INSERT -->
-      <!---->
-      <!--menu-->
+
       <nav class="navbar navbar-lg navbar-expand-lg navbar-transparant navbar-dark navbar-absolute w-100 bg-white">
          <div class="container">
             <!--<a class="navbar-brand" href="index.html">Robust.</a>-->
@@ -313,63 +194,31 @@
                         </li>
                      </ul>
                      <p class="fmenu lh-30 mt-2">
-                        <a href="https:https://www.instagram.com/tea_wheeler/">
-                           <svg
-                              class="svg-inline--fa fa-instagram fa-w-14 fa-lg sicon mr-10" aria-hidden="true"
-                              data-prefix="fab" data-icon="instagram" role="img"
-                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                              <path fill="currentColor"
-                                 d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
-                              </path>
-                           </svg>
-                           <!-- <i class="fab fa-instagram fa-lg sicon mr-10"></i> -->
-                        </a>
-                        <a href="https:https://twitter.com/teawheeler_offl">
-                           <svg
-                              class="svg-inline--fa fa-twitter fa-w-16 fa-lg sicon mr-10" aria-hidden="true"
-                              data-prefix="fab" data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 512 512" data-fa-i2svg="">
-                              <path fill="currentColor"
-                                 d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z">
-                              </path>
-                           </svg>
-                           <!-- <i class="fab fa-twitter fa-lg sicon mr-10"></i> -->
-                        </a>
-                        <a
-                           href="https:https://www.facebook.com/profile.php?id=100080041909248">
-                           <svg
-                              class="svg-inline--fa fa-facebook fa-w-14 fa-lg sicon mr-10" aria-hidden="true"
-                              data-prefix="fab" data-icon="facebook" role="img" xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 448 512" data-fa-i2svg="">
-                              <path fill="currentColor"
-                                 d="M448 56.7v398.5c0 13.7-11.1 24.7-24.7 24.7H309.1V306.5h58.2l8.7-67.6h-67v-43.2c0-19.6 5.4-32.9 33.5-32.9h35.8v-60.5c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9h-58.4v67.6h58.4V480H24.7C11.1 480 0 468.9 0 455.3V56.7C0 43.1 11.1 32 24.7 32h398.5c13.7 0 24.8 11.1 24.8 24.7z">
-                              </path>
-                           </svg>
-                           <!-- <i class="fab fa-facebook fa-lg sicon mr-10"></i> -->
-                        </a>
-                        <a href="">
-                           <svg class="svg-inline--fa fa-youtube fa-w-18 fa-lg sicon mr-10"
-                              aria-hidden="true" data-prefix="fab" data-icon="youtube" role="img"
-                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
-                              <path fill="currentColor"
-                                 d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z">
-                              </path>
-                           </svg>
-                           <!-- <i class="fab fa-youtube fa-lg sicon mr-10"></i> -->
-                        </a>
-                        <a
-                           href="https:https://www.linkedin.com/in/tea-wheeler-21308a237/">
-                           <svg
-                              class="svg-inline--fa fa-linkedin fa-w-14 fa-lg sicon" aria-hidden="true"
-                              data-prefix="fab" data-icon="linkedin" role="img" xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 448 512" data-fa-i2svg="">
-                              <path fill="currentColor"
-                                 d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z">
-                              </path>
-                           </svg>
-                           <!-- <i class="fab fa-linkedin fa-lg sicon"></i> -->
-                        </a>
-                     </p>
+                    <a href="https://www.instagram.com/tea_wheeler/">
+                        <svg
+                            class="svg-inline--fa fa-instagram fa-w-14 fa-lg sicon mr-10" aria-hidden="true"
+                            data-prefix="fab" data-icon="instagram" role="img"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
+                            <path fill="currentColor"
+                                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
+                            </path>
+                        </svg>
+                        <!-- <i class="fab fa-instagram fa-lg sicon mr-10"></i> -->
+                    </a>
+                    
+                    <a
+                        href="https://www.facebook.com/teawheelers">
+                        <svg
+                            class="svg-inline--fa fa-facebook fa-w-14 fa-lg sicon mr-10" aria-hidden="true"
+                            data-prefix="fab" data-icon="facebook" role="img" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512" data-fa-i2svg="">
+                            <path fill="currentColor"
+                                d="M448 56.7v398.5c0 13.7-11.1 24.7-24.7 24.7H309.1V306.5h58.2l8.7-67.6h-67v-43.2c0-19.6 5.4-32.9 33.5-32.9h35.8v-60.5c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9h-58.4v67.6h58.4V480H24.7C11.1 480 0 468.9 0 455.3V56.7C0 43.1 11.1 32 24.7 32h398.5c13.7 0 24.8 11.1 24.8 24.7z">
+                            </path>
+                        </svg>
+                        <!-- <i class="fab fa-facebook fa-lg sicon mr-10"></i> -->
+                   
+                    </p>
                   </div>
                </div>
             </div>
